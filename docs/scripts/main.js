32a4201d41f3d699ee9fbdb3a00741e4a4a7f949
Waylon Carrier Nunes <wayloncn@gmail.com>
2020-10-06 12:04:20 -0400

    implemented the animated menu for mobile.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,17 +1,32 @@
 "use strict";
 const hamburger = document.querySelector('#hamburger');
-let subnavOpened = false;
-hamburger.addEventListener('click', () => {
+const mainMenuNav = document.querySelector('#mainMenu');
+let mainMenuOpen = false;
+hamburger.addEventListener('click', (event) => {
+    event.stopPropagation();
     // first time clicked
-    if (subnavOpened) {
-    }
-    if (subnavOpened) {
+    if (mainMenuOpen) {
         hamburger.classList.replace('opened', 'closed');
+        mainMenuNav.classList.remove('open');
     }
     else {
         hamburger.classList.remove('closed');
         hamburger.classList.add('opened');
+        // mainMenuNav.style.display = 'block';
+        // window.setTimeout(() => {
+        //   mainMenuNav.style.right = '0';
+        // }, 10);
+        mainMenuNav.classList.add('open');
     }
-    subnavOpened = !subnavOpened;
+    mainMenuOpen = !mainMenuOpen;
+});
+window.addEventListener('click', (event) => {
+    console.log(event.target);
+    console.log(mainMenuNav);
+    if (event.target != mainMenuNav) {
+        hamburger.classList.replace('opened', 'closed');
+        mainMenuNav.classList.remove('open');
+        mainMenuOpen = false;
+    }
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,17 +1,34 @@
 const hamburger = document.querySelector('#hamburger') as HTMLDivElement;
-let subnavOpened = false;
+const mainMenuNav = document.querySelector('#mainMenu') as HTMLElement;
+let mainMenuOpen = false;
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', (event) => {
+  event.stopPropagation();
   // first time clicked
-  if (subnavOpened) {
-  }
-
-  if (subnavOpened) {
+  if (mainMenuOpen) {
     hamburger.classList.replace('opened', 'closed');
+    mainMenuNav.classList.remove('open');
   } else {
     hamburger.classList.remove('closed');
     hamburger.classList.add('opened');
+
+    // mainMenuNav.style.display = 'block';
+    // window.setTimeout(() => {
+    //   mainMenuNav.style.right = '0';
+    // }, 10);
+    mainMenuNav.classList.add('open');
   }
 
-  subnavOpened = !subnavOpened;
+  mainMenuOpen = !mainMenuOpen;
+});
+
+window.addEventListener('click', (event) => {
+  console.log(event.target);
+  console.log(mainMenuNav);
+
+  if (event.target != mainMenuNav) {
+    hamburger.classList.replace('opened', 'closed');
+    mainMenuNav.classList.remove('open');
+    mainMenuOpen = false;
+  }
 });
